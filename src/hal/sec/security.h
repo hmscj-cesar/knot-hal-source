0f2bd93de019c0c45c49b42214e38ad252e60167
Hélmiton <hmscj@cesar.org.br>
2017-08-28 11:41:27 -0300

    nRF24: Add Security Module's core files
    Added main files for our security module with specific versions for
    each side (security for gw and security_ino for thing). They provide
    encryption and decryption functions (AES 256 CBC) using ECC keys and
    Diffie–Hellman protocol for key agreement.

---
+++ src/hal/sec/security.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <openssl/obj_mac.h>
+#include <openssl/ec.h>
+#include <openssl/evp.h>
+#include <openssl/bio.h>
+#include <openssl/err.h>
+#include <openssl/pem.h>
+#include <openssl/aes.h>
+#include <openssl/rand.h>
+#include <openssl/conf.h>
+#include <string.h>
+#include <stdint.h>
+#include <assert.h>
+
+#define secp128r1	16
+#define secp192r1	24
+#define secp256r1	32
+#define secp384r1	48
+#define ECC_CURVE	secp256r1
+
+#if (ECC_CURVE != secp192r1 && ECC_CURVE != secp256r1 \
+	&& ECC_CURVE != secp384r1)
+	#error "ERROR_CURVE_NOT_DEFINED"
+#endif
+
+#define NUM_ECC_DIGITS ECC_CURVE
+
+int encrypt(uint8_t *plaintext, size_t plaintext_len,
+			uint8_t *key, uint8_t *iv);
+int decrypt(uint8_t *ciphertext, size_t ciphertext_len,
+			uint8_t *key, uint8_t *iv);
+int derive_secret(uint8_t stpx[], uint8_t stpy[], uint8_t lcpriv[],
+		uint8_t lcpx[], uint8_t lcpy[], uint8_t secret[], uint8_t *iv);
+int generate_keys(uint8_t *keys);
